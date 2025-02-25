e7cc6409a126805facd48c0d4bc9e749afa6487c
Rado23-04 <hei.rado.2@gmail.com>
2025-02-25 15:39:08 +0300

    fix: route protected

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Nav from "./assets/components/Nav"
 import { ProtectedRoute } from "./assets/components/ProtectedRoute/ProtectedRoute";
+import Task from "./assets/components/Task";
 import Register from "./page/Register"
 import Sign from "./page/Sign"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -11,8 +12,10 @@ function App() {
         <Nav/>
             <Routes>
                 <Route path="/" element={<Register />} />
-                <Route element={<ProtectedRoute />}></Route>
-                <Route path='/save' element = {<Sign/>}/>
+                <Route element={<ProtectedRoute />}>
+                    <Route path='/task' element = {<Task/>}/>
+                    <Route path='/save' element = {<Sign/>}/>
+                </Route>
             </Routes>
         </Router>
     </>
